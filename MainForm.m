707a4bed3f7ddf9a03bd3203b90c44e2926f0cdd
Ivan <iv.mykh@gmail.com>
2018-12-25 11:41:51 +0200

    Added BCs

---
+++ MainForm.m
@@ -0,0 +1,167 @@
+function varargout = MainForm(varargin)
+% MAINFORM MATLAB code for MainForm.fig
+%      MAINFORM, by itself, creates a new MAINFORM or raises the existing
+%      singleton*.
+%
+%      H = MAINFORM returns the handle to a new MAINFORM or the handle to
+%      the existing singleton*.
+%
+%      MAINFORM('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MAINFORM.M with the given input arguments.
+%
+%      MAINFORM('Property','Value',...) creates a new MAINFORM or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before MainForm_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to MainForm_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help MainForm
+
+% Last Modified by GUIDE v2.5 25-Dec-2018 11:40:41
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @MainForm_OpeningFcn, ...
+                   'gui_OutputFcn',  @MainForm_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before MainForm is made visible.
+function MainForm_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to MainForm (see VARARGIN)
+
+% Choose default command line output for MainForm
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes MainForm wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = MainForm_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in computeBtn.
+function computeBtn_Callback(hObject, eventdata, handles)
+% hObject    handle to computeBtn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clc;
+
+tic;
+b0 = [
+    1 
+    1
+ ];
+% FiniteDifferences
+% Adjoint
+% DirectDiff
+q = GeneralProblem(b0);
+
+x0Bc =get(get(handles.x0ButtonGroup,'SelectedObject'),'String')
+xeBc = get(get(handles.xeButtonGroup,'SelectedObject'),'String')
+
+if (strcmp(x0Bc,'Dirichlet') && strcmp(xeBc,'Dirichlet'))
+    q.bcType = [Helper.Dirichlet; Helper.Dirichlet]
+elseif (strcmp(x0Bc,'Dirichlet') && strcmp(xeBc,'Neumann'))
+    q.bcType = [Helper.Dirichlet; Helper.Neumann]
+elseif (strcmp(x0Bc,'Neumann') && strcmp(xeBc,'Dirichlet'))
+    q.bcType = [Helper.Neumann; Helper.Dirichlet]
+elseif (strcmp(x0Bc,'Neumann') && strcmp(xeBc,'Neumann'))
+    q.bcType = [Helper.Neumann; Helper.Neumann]
+end
+
+% if strcmp(x0Bc,'Dirichlet')
+%     q.bcType(:,1) = Helper.Dirichlet;
+% else
+%     q.bcType(:,1) = Helper.Neumann;
+% end
+
+q.bcType
+
+q.optimize(false, true);
+%q.direct()
+q.b
+q.criteria()
+q.constraint()
+toc;
+
+plotResults(q, 'Title')
+
+
+
+function xeBcValue_Callback(hObject, eventdata, handles)
+% hObject    handle to xeBcValue (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of xeBcValue as text
+%        str2double(get(hObject,'String')) returns contents of xeBcValue as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function xeBcValue_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to xeBcValue (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function x0BcValue_Callback(hObject, eventdata, handles)
+% hObject    handle to x0BcValue (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of x0BcValue as text
+%        str2double(get(hObject,'String')) returns contents of x0BcValue as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function x0BcValue_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to x0BcValue (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
---
+++ plotResults.m
@@ -0,0 +1,36 @@
+function plotResults(problem, myTitle)    
+    x = linspace(problem.x0, problem.xE, 500);
+    y = problem.y(x);
+
+    tName = '';
+    if nargin == 2
+        tName = [myTitle ': '];
+    end
+    figure('Name', sprintf('%sPsi0 = %f; Psi1 = %f', tName,...
+       problem.criteria(), problem.constraint()));
+
+    subplot(3, 1, 1);
+    hold on
+    plot(x,problem.u(x));
+    hold off
+    xlabel(' ')
+    title('u(x)');
+
+    subplot(3, 1, [2 3]); 
+    hold on
+    plot(x, y);
+
+    optRange = problem.Omega(problem.optO)';
+    line(optRange, repmat(problem.yd, size(optRange)),...
+        'Color', 'r', 'LineStyle', '-.');
+
+    limRange = problem.Omega(Helper.OtherIndex(problem.optO))';
+    line(limRange, repmat(problem.yMax, size(limRange)),...
+        'Color', 'b', 'LineStyle', '--');
+
+    line(repmat(problem.Omega(1),2,1), repmat(get(gca,'YLim'),2,1)',...
+        'Color', 'm', 'LineStyle', ':');
+    hold off
+    title('y(x)');
+    xlabel('x');
+end
